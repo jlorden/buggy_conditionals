54a4e0be4e0583ea82aaf405c2d91028283ff50d
Jlorden <Jamie.lorden@gmail.com>
2020-02-17 22:17:47 -0600

    Fixed Errors

--- bugs.js
+++ bugs.js
@@ -1,23 +1,18 @@
 var firstName = "Robert";
 var nickName = "Bob";
-var petName = Fluffy;
-xar petType = "Turtle";
-if(name == "Robert"){
-    console.log("Hi Robert");
+var petName = "Fluffy";
+var petType = "Turtle";
+if (firstName == "Robert"){
+    console.log("Hi, Robert");
 }
-else if(name == "Bob"){
-    console("Hi Bob");
+else if(nickName == "Bob"){
+    console.log("Hi,Bob");
 }
-else(name != "Robert"){
-    console.log("Hello Stranger");
-}
-if petType == "Dog" {
+if (petType == "Dog") {
     console.log("Time to play fetch");
 }
-if(petType = "Cat"){
+if (petType == "Cat") 
     console.log("Time to play with a laser pointer");
-}
-else
-    console.log(Time to play with my pet);
-}
 
+if (petType == "Turtle")
+    console.log("Time to play with my pet");
